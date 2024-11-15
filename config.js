03051426335fd3a1a96cd52363423d24e7e01b3f
teentige <teentiger12105@gmail.com>
2024-11-15 11:43:06 +0300

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 
 module.exports = {
   //==========================================- MAIN - CONFIGS -==================================================================
-  SESSION_ID: process.env.SESSION_ID || "Put Session Id",
+  SESSION_ID: process.env.SESSION_ID || "AGUQSTgC#yjNvqxCRtwuRVYtw4iZugh3eltf6VqgktHS8gpAUanM",
   // ADD Your Session Id 
   MONGODB: process.env.MONGODB || "mongodb+srv://kulathungaasitha319:yjHB0DvFfStNfwPS@cluster0.3oijd.mongodb.net/",
     // ADD Your MongoDB Database URL
